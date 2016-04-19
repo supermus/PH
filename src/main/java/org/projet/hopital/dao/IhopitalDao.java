f0313fd5b8640052247ab38ffe5c7e9afb87a881
Amine-minou <Amine-minou@Amineminou-PC>
2016-04-19 13:54:54 +0200

    DAO CLASSES

--- src/main/java/org/projet/hopital/dao/IhopitalDao.java
+++ src/main/java/org/projet/hopital/dao/IhopitalDao.java
@@ -1,5 +1,8 @@
 package org.projet.hopital.dao;
 
+import  org.projet.hopital.entities.*;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -7,8 +10,27 @@ import javax.persistence.PersistenceContext;
 import org.projet.hopital.entities.Compte;
 
 
+
+
 public interface IhopitalDao {
 	
 	
+	public DossierPatient rechercherDossier (long idDossierPatient);
+	
+	public List<DossierPatient> listeDossier();
+	
+	public void modifierDossier(long idDossierPatient);
+	
+	public boolean ajouterDossier(DossierPatient dossier);	
+	
+	public boolean ajouterRdv(DossierPatient dp, RendezVous rdv);
+	
+	public void modifierRdv(DossierPatient dp,RendezVous rdv);
+	
+	public boolean supprimerRdv(DossierPatient dp,RendezVous rdv);
+	
+	public void ajouterEmployee(Employee employee);
+	
+	public void attribuerRole(Employee employee, Role r);
 	
 }	
--- src/main/java/org/projet/hopital/dao/hopitalDaoImpl.java
+++ src/main/java/org/projet/hopital/dao/hopitalDaoImpl.java
@@ -8,6 +8,60 @@ import javax.persistence.PersistenceContext;
 import org.projet.hopital.entities.*;
 
 public class hopitalDaoImpl implements IhopitalDao{
+
+	@Override
+	public DossierPatient rechercherDossier(long idDossierPatient) {
+		
+		return null;
+	}
+
+	@Override
+	public void ajouterEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attribuerRole(Employee employee, Role r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<DossierPatient> listeDossier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void modifierDossier(long idDossierPatient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean ajouterDossier(DossierPatient dossier) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ajouterRdv(DossierPatient dp, RendezVous rdv) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void modifierRdv(DossierPatient dp, RendezVous rdv) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean supprimerRdv(DossierPatient dp, RendezVous rdv) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 	
 }
