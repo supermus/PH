6b6a2dfc4234ad25a271be949520e56f85142ca0
Supermus <el-madi@live.fr>
2016-04-20 18:26:44 +0200

    l'interface IhopitalMetier

---
+++ src/main/java/org/projet/hopital/metier/IHopitalMetier.java
@@ -0,0 +1,29 @@
+package org.projet.hopital.metier;
+
+import java.util.List;
+
+import org.projet.hopital.entities.DossierPatient;
+import org.projet.hopital.entities.Employee;
+import org.projet.hopital.entities.RendezVous;
+import org.projet.hopital.entities.Role;
+
+public interface IHopitalMetier {
+	
+	public DossierPatient rechercherDossier (long idDossierPatient);
+	
+	public List<DossierPatient> listeDossierPatient();
+	
+	public void modifierDossier(DossierPatient dp);
+	
+	public long ajouterDossier(DossierPatient dossier);	
+	void ajouterRdv(long idDossierPatient, RendezVous rdv);
+	
+	public void modifierRdv(DossierPatient dp,RendezVous rdv);
+	
+	public boolean supprimerRdv(DossierPatient dp,RendezVous rdv);
+	
+	public long ajouterEmployee(Employee employee);
+	
+	public void attribuerRole(long idEmploye, Role r);
+	
+}
